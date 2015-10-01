466c0cf08b9b06ed7f5fa5d227085c7c176cf0c5
bselva <brunoselva@gmail.com>
2015-09-30 21:30:05 -0700

    Fix database folder

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var tungus   = require('tungus');
 var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port  	 = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
+var port  	 = process.env.PORT || 2222; 				// set the port
+//var database = require('./config/database'); 			// load the database config
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
-mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+mongoose.connect('tingodb://'+__dirname+'/ssnocdb/'); 	// connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
