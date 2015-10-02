f4233adf529c18c757d4690b15737ea820ce0e37
bselva <brunoselva@gmail.com>
2015-10-02 14:11:03 -0700

    API calls

--- app/models/models.js
+++ app/models/models.js
@@ -1,6 +1,4 @@
 var tungus = require('tungus');
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('Todo', {
-	text : {type : String, default: ''}
-});
+module.exports = mongoose.model('Member', {name: String, password: String, status: Number});
