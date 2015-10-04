dc6c3f7db93877867579b8d59fce2904a2664cf8
Livani Pillay <livanipillay@gmail.com>
2015-10-03 20:56:45 -0700

    Updated angular controller

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -1,43 +1,34 @@
-angular.module('ssnocController', [])
+angular.module('mainController', [])
 
 	// inject the Todo service factory into our controller
 	.controller('mainController', ['$scope','$http','SsnocFactory', function($scope, $http, SsnocFactory) {
 		$scope.member = {};
 		$scope.directory = {};
+		$scope.sortType =""
 		$scope.loading = true;
-
-		var username = typeusername; //User typein
-		var password = typepassword; //user typein pass
 
 		// GET =====================================================================
 		// when landing on the page, get all todos and show them
 		// use the service to get all the todos
-		SsnocFactory.getDirectory()
-			.success(function(data) {
-				$scope.directory = data;
-				$scope.loading = false;
-			});
 
 		$scope.login = function(){
 
 			$scope.loading = true;
-			if(isExistingMember()){
-				if($scope.validateLoginDetails){
+			if($scope.isExistingMember){
+				if(validateLoginDetails()){
 					updateStatus();		
 				}
-			}
-				
+			}	
 			else{
-				if($scope.validateSignInDetails){
-					updateStatus();
-				}
-			}
-			// 	}
-			// }
-			// else
-			// {
 
-			// }
+			}
+		}
+
+		$scope.signIn = function()
+		{
+			if(validateSignInDetails()){
+				updateStatus();
+			}
 		}
 
 		$scope.logout = function(){
@@ -45,7 +36,7 @@ angular.module('ssnocController', [])
 			goOffline();
 		}
 
-		getDirectory = function()
+		function getDirectory()
 		{	
 			$scope.loading = true;
 			SsnocFactory.getDirectory()
@@ -56,40 +47,42 @@ angular.module('ssnocController', [])
 			  //   "fbwolf": 0,
 			  // }
 				$scope.directory = data;
-        //form two lists
-				var onlinelist=[];
-				var offlinelist=[];
-				for (var name in data){
-					var status=data[name];
-					if ( status != 0){
-						// add name into list
-						onlinelist.push(name);
-					}
-					else {
-						offlinelist.push(name);
-					}	 	
-				}
+				$scope.loading = false;
+
+				//@angela angular will handle the sort for you. 
+       	
+      //  	        angular.forEach($scope.directory, function (name) {
+	     //           	var status=data[name];
+						// if ( status != 0){
+						// 	// add name into list
+						// 	onlinelist.push(name);
+						// }
+						// else {
+						// 	offlinelist.push(name);
+						// }	 
+	     //        });
 
 				 // sort list and merge list
-				 onlinelist.sort();
-				 offlinelist.sort();
-         var list = onlinelist.concat(offlinelist);
+				 // onlinelist.sort();
+				 // offlinelist.sort();
+     //    	 	 var list = onlinelist.concat(offlinelist);
 
-				 // acccoding to list, display it
-				 angular.element($('#directory')).empty();
-            for( var i=0; i<list.length; i++){
-              $('#directory').append(
-                $('<li>').html(list[i].name)
-                );
-           }
+				 // // acccoding to list, display it
+				 // angular.element($('#directory')).empty();
 
-				$scope.loading = false;
+				 // angular.forEach(list, function(name){
+				 // 	$('#directory').append(
+	    //             $('<li>').html(name);
+	    //             );
+				 // });
+
+				
 			});	
 
 		}
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
-		createMember = function() {
+		function createMember() {
 				$scope.loading = true;
 
 				// call the create function from our service (returns a promise object)
@@ -100,48 +93,54 @@ angular.module('ssnocController', [])
 					});
 		};
 
+		function getMember(){
+			$scope.loading = true;
+			SsnocFactory.getMember($scope.member.username)
+			.success(function(data){
 
-		isExistingMember = function(){
-			SsnocFactory.getMember($username)
-				.success(function(data){
-					//check existing member from user typein username with DB 
-					if(data != undefined)
-					{
-						$scope.member = data;
-						return true;
-					}else
-					{
-						$message = "no existing member";
-						return false;
-					}
-				});
+				$scope.loading = false;
+				return data;
+			}
 		}
 
-		$scope.validateLoginDetails = function(){
-			if (  $password == $scope.member.password){
-			  	return true;
-			  } 
+		$scope.isExistingMember = function(){
+			if(getMember()!=undefined)
+			{
+				return true;
+			}
+			else
+			{
+				$scope.message = "no existing member";
+				return false;
+			}
+
+		}
+
+		function validateLoginDetails(){
+
+			if(getMember().password == $scope.member.password)
+			{
+				return true;
+			}
 			else {
 				 $message = "wrong infomation";
 				 return false;		 
 			}
-
-
 		}
 
-		$scope.validateSignInDetails = function(){
+		function validateSignInDetails(){
 			//usecase requirements: https://drive.google.com/file/d/0B7mdj-x_n_iSX3Y4Q2FoallpMEE/view
 			var reservedlists = ["backup", "banner", "banners", "bin", "billing", "blog", "blogs", "board", "bot"];
-			if ( $username.length < 3 || reservedlists.indexOf($username) != -1 ){
-				$message = "Please provide a different username.";
+			if ($scope.member.username.length < 3 || reservedlists.indexOf($scope.member.username) != -1 ){
+				$scope.message = "Please provide a different username.";
 				return false;
 			}
-			if ( $password_1.length < 4){
-				$message = "Please provide a different password, the password should include atleat 4 characters.";
+			if ($scope.member.password.length < 4){
+				$scope.message = "Please provide a different password, the password should include atleat 4 characters.";
 				return false;
 			}
-			if ( $password_1 != $password_2 ){
-				$message = "the two passwords dosen't match.";
+			if ($scope.member.password != $scope.member.confirmPassword){
+				$scope.message = "the two passwords dosen't match.";
 				return false;
 			}
 			else {
@@ -150,25 +149,11 @@ angular.module('ssnocController', [])
 		}
 
 
-		// goOnline = function(){
-		// 	//@angela validate here
-		// 	if( $scope.member.status != 0){
-		// 		updateStatus();
-		// 	}		
-		// }
-
-		// goOffline = function(){
-		// 	//@angela please add validation
-		// 	if ( $scope.member.status == 0){
-		// 		updateStatus();
-		// 	}			
-		// }
-
-//update status send no
-		updateStatus = function(member){
+		//update status send no
+		function updateStatus(member){
 			SsnocFactory.updateStatus(member).success(
 				function(data){
-			 		// $scope.member = data;
+			 		$scope.member = data;
 			 		console.log(data);
 			 		$scope.loading = false;
 			 	}
