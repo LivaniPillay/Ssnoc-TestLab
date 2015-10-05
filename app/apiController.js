cb0b7389a4de8e8253f8526c20ae65b11723043f
bselva <brunoselva@gmail.com>
2015-10-05 10:58:02 -0700

    Fix API path

--- app/apiController.js
+++ app/apiController.js
@@ -79,6 +79,6 @@ module.exports = function(app) {
 	});
 
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile(__dirname + '../public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
 };
